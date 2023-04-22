ceef25736ab9118785ee3d2a28df1f579d8a9fdf
Marietta <125894364+Mari3tt4@users.noreply.github.com>
2023-04-23 00:09:35 +0800

    Add files via upload

---
+++ perform.js
@@ -0,0 +1,27 @@
+let age = prompt("Hello! How old are?:")
+
+if(age>= 25)
+
+{
+    alert("Welcome to Counting Numbers")
+
+}
+
+else
+{
+    alert("Sorry, it's only for 25 years old and above") ;
+}
+
+let number = prompt("Enter a number");
+if(number < 100){
+    alert("You entered 100 below");
+}
+else
+{
+    alert("You entered more than 100");
+}
+
+
+
+
+  
